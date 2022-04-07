b0a55f072af5ad08902f234368eb245d1020526c
ccgg1290local <ccgg1290@gmail.com>
2022-04-06 22:02:51 -0500

    agregamos componente empleado automaticamente y lo utilizamos dentro de empleados -- este mensaje lo modificamos con rebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmpleadosComponent } from './empleados/empleados.component';
+import { EmpleadoComponent } from './empleado/empleado.component';
 
 @NgModule({
   declarations: [
-    AppComponent, EmpleadosComponent
+    AppComponent, EmpleadosComponent,  EmpleadoComponent
   ],
   imports: [
     BrowserModule,
