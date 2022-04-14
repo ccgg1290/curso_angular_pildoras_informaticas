d9bb398ed381b032c30c690fcf6a3133d6a2a8db
ccgg1290local <ccgg1290@gmail.com>
2022-04-14 12:36:50 -0500

    video 7; interpolacion, se muestra como se pueden crear texto dinamico-tabn vimos operador ternario

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -6,6 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./empleado.component.css']
 })
 export class EmpleadoComponent implements OnInit {
+  nombre="cristian ";
+  apellido="gualtero";
+  // es poisble acceder a estas variables desde otra clase, 
+  // pero tabn podemos encapsula rlos datos y acceder a estos a traves de metodos getter setter
+  private edad=5;
+  empresa="Movii";
+
+  
+ getEdad(){
+  // metodo getter
+  return this.edad;
+ }
+
 
   constructor() { }
 
